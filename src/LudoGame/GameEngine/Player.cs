fddfcbdc917362544adddb3eeb83f3a50ca602b3
Johan Wassberg <johanwassberg@icloud.com>
2019-01-17 21:27:40 +0100

    #Dice - Added modifier sealed to prevent inheritance. - Added XML documentation comments.
    #Piece
    - Added XML documentation comments.
    - Bug fix: GetAbsolutePosition() now returns 0 when position is 0
    - Replaced conditional operator syntax with an IF ELSE statement due to the complexity in which the additional condition (above) provided.
    
    #Player
    - Added XML documentation comments.
    
    #Program
    - Renamed ListPieces() to ListAllPieces().
    - Added method ListPlayerPieces(in: Player) which prints list of a player's pieces in which the player can choose from.

--- src/LudoGame/GameEngine/Player.cs
+++ src/LudoGame/GameEngine/Player.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace GameEngine
 {
+    /// <summary>
+    /// Represents the participant in the game.
+    /// </summary>
     public class Player
     {
         // Spelarens pjäser ligger här!
         public List<Piece> Pieces { get; set; }
         private int number;
 
+        /// <summary>
+        /// Initialises a new Player.
+        /// </summary>
+        /// <param name="number">The player's identity.</param>
         public Player(int number)
         {
             Pieces = new List<Piece>(4);
